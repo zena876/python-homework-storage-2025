afb3d5abac8a9ea51da4628f41c44a4bb31d0aeb
zena876 <nzena605@gmail.com>
2025-09-26 11:25:24 +0300

    Update task01.py

--- Фамилия Имя Отчество/LAB01/task01.py
+++ Фамилия Имя Отчество/LAB01/task01.py
@@ -1,4 +1,12 @@
-#!/usr/env/python
-
 if __name__ == '__main__':
-    ...
+    number = []
+    while len(number) < 10:
+        temp = input("введите число: ")
+        try:
+            temp_int = int(temp)
+            number.append(int(temp))
+        except ValueError:
+            print("ошика")
+    print(f"максимальное - max(i for i in number)")
+    print(f"минимальное - min(i for i in number)")
+    print(f"сумма - sum(i for i in number)")
