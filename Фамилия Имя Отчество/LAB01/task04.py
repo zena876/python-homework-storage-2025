6b694c44c3304fc03e382306aa020d5a2c5991d9
zena876 <nzena605@gmail.com>
2025-10-10 10:48:29 +0300

    Update task04.py

--- Фамилия Имя Отчество/LAB01/task04.py
+++ Фамилия Имя Отчество/LAB01/task04.py
@@ -1,1 +1,10 @@
-
+lin = [52, 2, 5, 52, 43, 67, 45344, 228, 25552, 252525, 5, 2, 52]
+if __name__ == '__main__':
+    diznuts = {}
+    for i in lin:
+        if i not in diznuts:
+            diznuts[i] = 1
+        else:
+            diznuts[i] += 1
+    for su in diznuts:
+        print(f"chislo '{su}' vstrechaetsa: {diznuts.get(su)} raz")
