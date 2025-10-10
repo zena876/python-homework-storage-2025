a199247a6094f6966f8f36cc4c9f211e7ad14469
zena876 <nzena605@gmail.com>
2025-10-10 10:27:51 +0300

    Update task01.py

--- Фамилия Имя Отчество/LAB01/task01.py
+++ Фамилия Имя Отчество/LAB01/task01.py
@@ -7,6 +7,17 @@ if __name__ == '__main__':
             number.append(int(temp))
         except ValueError:
             print("ошика")
-    print(f"максимальное - max(i for i in number)")
-    print(f"минимальное - min(i for i in number)")
-    print(f"сумма - sum(i for i in number)")
+    if number:
+        maxi = number[0]
+        mini = number[0]
+        total = 0
+        for num in number:
+            if num > maxi:
+                maxi = num
+            if num < mini:
+                mini = num
+            total += num
+
+        print(f'maximum: {maxi}, minimum: {mini}, summa: {total}')
+    else:
+        print('pusto')
