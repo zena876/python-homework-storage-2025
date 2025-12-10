8a5cb6049f1939af2be07e55c28ba88fac88d4b2
zena876 <nzena605@gmail.com>
2025-12-10 22:06:43 +0300

    Update task3.py

--- Наторова Евгения Михайловна/Lab08/task3.py
+++ Наторова Евгения Михайловна/Lab08/task3.py
@@ -1,10 +1,6 @@
 import re
 
-def splt_words():
+if __name__== "__main__":
     text = input("Введите строку для разбития на слова в формат: ")
     result = re.split(r'[.,!?-]', text)
     print("Список слов без пустых элементов:", result)
-
-if __name__== "__main__":
-    splt_words()
-    
