4c02374cc8da60e149a43df746061614c6f9f865
zena876 <nzena605@gmail.com>
2025-12-10 22:07:47 +0300

    Update task 2.py

--- Наторова Евгения Михайловна/Lab08/task 2.py
+++ Наторова Евгения Михайловна/Lab08/task 2.py
@@ -1,12 +1,6 @@
 import re
 
-def replace_dates():
+if __name__== "__main__":
     text = input("Введите строку с датами в формате ДД.ММ.ГГГГ: ")
     result = re.sub(r'\d{2}.\d{2}.\d{4}', 'DD.MM.YYYY', text)
     print("Результат замены:", result)
-
-
-if __name__== "__main__":
-    replace_dates()
-    
-
